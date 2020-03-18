6d2ac7454a818704ba1788e9f9e892140a6cc91c
Andy Freidenfelds <andy.freidenfelds@grainger.com>
2020-03-18 09:48:25 -0500

    use "node" for installing latest in nvm

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-nvm install latest
+nvm install node
 
 echo `node -v && npm -v`
