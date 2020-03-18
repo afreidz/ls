41db10cd4081cf50e5b3536cb4e70e2bfdaf9b97
Andy Freidenfelds <andy.freidenfelds@grainger.com>
2020-03-18 10:37:31 -0500

    adds partition prompts

--- install.sh
+++ install.sh
@@ -1,12 +1,24 @@
 #! /usr/bin/env bash
 
-# Install Nodejs
-echo 'Installing NodeJS'
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+# Partitioning
+echo $(lsblk)
+read -p "Which disk do you want to install on?" disk
+read -p "What size for the boot partition?" bootize
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-nvm install node
-
-echo `node -v && npm -v`
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${disk}
+  o # clear the in memory partition table
+  n # new partition
+  p # primary partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+  +${bootsize} # boot parttion
+  n # new partition
+  p # primary partition
+  2 # partion number 2
+    # default, start immediately after preceding partition
+    # default, extend partition to end of disk
+  a # make a partition bootable
+  1 # bootable partition is partition 1
+  p # print the in-memory partition table
+  q # and we're done
+EOF
